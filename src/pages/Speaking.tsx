7407a853695eddcee80c864ba170d6cf73881ccf
career-capital <nisrexach@gmail.com>
2026-01-02 02:46:33 -0600

    Updated Speaking.tsx

--- src/pages/Speaking.tsx
+++ src/pages/Speaking.tsx
@@ -267,8 +267,7 @@ export default function Speaking({ onNavigate }: SpeakingProps) {
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                  />
+                    allowFullScreen></iframe>
                 </div>
               </div>
             </div>
