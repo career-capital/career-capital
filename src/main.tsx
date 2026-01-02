737f34db00665b98b5119a397e5bd1898166dbd6
career-capital <nisrexach@gmail.com>
2026-01-01 23:07:14 -0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+console.log('=== MAIN.TSX LOADING ===');
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+console.log('=== ABOUT TO RENDER APP ===');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+console.log('=== APP RENDERED ===');
